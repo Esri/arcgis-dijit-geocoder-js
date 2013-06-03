2b6d9a15b96cdb16078fefbd2a0be05f16b56f7f
Matt Driscoll <mdriscoll@esri.com>
2013-06-03 10:47:41 -0700

    no message

--- src/Geocoder.js
+++ src/Geocoder.js
@@ -9,10 +9,10 @@ define([
     "dojo/keys",
     "dojo/on",
     "dojo/query",
-	"dojo/i18n!esri/nls/jsapi",
-    "dojo/text!esri/dijit/templates/Geocoder.html",
-    //"dojo/i18n!esri/nls/jsapi",
+	//"dojo/i18n!esri/nls/jsapi",
     //"dojo/text!esri/dijit/templates/Geocoder.html",
+    "dojo/i18n!./nls/jsapi",
+    "dojo/text!./templates/Geocoder.html",
     "dojo/uacss",
 
     "dijit/_OnDijitClickMixin",
